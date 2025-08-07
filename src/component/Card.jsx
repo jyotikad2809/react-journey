2868e5c67d84e7af6a15ae8aac42f05ad7662125
jyoti kad sharama <jyotikad2809@gmail.com>
2025-08-07 19:20:14 +0530

    feat: send dummy data as props to child component

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
 import "../style.css";
 import Chip from "./Chip";
 
-const skills = [
-  { id: 0, label: "aigkjgxkd" },
-  { id: 1, label: "ai" },
-  { id: 2, label: "ai" },
-];
-
-export default function Card({ title }) {
+export default function Card({ title, skills }) {
   return (
     <div className="card">
       <h3>{title}</h3>
-      <div className="skills-container">
-        <h5>Skills:</h5>
-        {skills.map((skill) => (
-          <Chip skill={skill.label} key={skill.id} />
-        ))}
+      <div className="skill-wrapper">
+        <span className="skill-title">Skills:</span>
+        <div className="skills-container">
+          {skills.map((skill) => (
+            <Chip skill={skill} key={skill} />
+          ))}
+        </div>
       </div>
     </div>
   );
