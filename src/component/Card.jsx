46217302bc0534ccc0b8f0c15000d81d2e61e827
jyoti kad sharama <jyotikad2809@gmail.com>
2025-08-07 18:53:34 +0530

    feat: import component chip

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,2 +1,23 @@
 import { useState } from "react";
-export default function Card() {}
+import "../style.css";
+import Chip from "./Chip";
+
+const skills = [
+  { id: 0, label: "aigkjgxkd" },
+  { id: 1, label: "ai" },
+  { id: 2, label: "ai" },
+];
+
+export default function Card({ title }) {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <div className="skills-container">
+        <h5>Skills:</h5>
+        {skills.map((skill) => (
+          <Chip skill={skill.label} key={skill.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
