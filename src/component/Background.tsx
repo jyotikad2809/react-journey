8ab8cb5909bdad3fd40fbe5505112987f975989d
jyoti kad sharama <jyotikad2809@gmail.com>
2025-08-08 13:31:46 +0530

    fix:add style  background cover not repeat

--- src/component/Background.tsx
+++ src/component/Background.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import backgroundImage from "../assets/ai-journey-bg.svg";
-
+//add one typescript for one demo purpose
+//other code base is in javascript
+//for showcase purpose only
 const BackgroundComponent = ({ children }: { children: React.ReactNode }) => {
   const styles: React.CSSProperties = {
     backgroundImage: `url(${backgroundImage})`,
     backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
     minHeight: "100vh",
     display: "flex",
     flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    paddingLeft: "200px",
-    paddingRight: "200px",
   };
 
   return <div style={styles}>{children}</div>;
